1c85b70888a7d1883513fc314b99df6285bee25e
MansurHaitukaiev <9101447@stud.op.edu.ua>
2025-10-05 20:58:16 +0200

    Lab4 completed 2try

---
+++ src/main/java/ua/opnu/java/inheritance/account/Credit.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package ua.opnu.java.inheritance.account;
+
+public class Credit {
+    private int balance;
+
+    public Credit(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+}
